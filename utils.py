113438f84932b36cd0c2bdc32745666e124e36c2
Ivan Savov <ivan.savov@gmail.com>
2023-03-12 14:37:43 -0400

    mv helper funcitons to .py file

---
+++ utils.py
@@ -0,0 +1,123 @@
+# LE Kaggle sample code
+# see https://www.kaggle.com/competitions/learning-equality-curriculum-recommendations
+from IPython.display import display
+from IPython.display import Markdown
+import pandas as pd
+from pathlib import Path
+
+
+
+# load the data into pandas dataframes
+data_dir = Path('learning-equality-curriculum-recommendations')
+topics_df = pd.read_csv(data_dir / "topics.csv", index_col=0).fillna({"title": "", "description": ""})
+content_df = pd.read_csv(data_dir / "content.csv", index_col=0).fillna("")
+correlations_df = pd.read_csv(data_dir / "correlations.csv", index_col=0)
+
+
+
+def print_markdown(md):
+    display(Markdown(md))
+
+    
+class Topic:
+    def __init__(self, topic_id):
+        self.id = topic_id
+
+    @property
+    def parent(self):
+        parent_id = topics_df.loc[self.id].parent
+        if pd.isna(parent_id):
+            return None
+        else:
+            return Topic(parent_id)
+
+    @property
+    def ancestors(self):
+        ancestors = []
+        parent = self.parent
+        while parent is not None:
+            ancestors.append(parent)
+            parent = parent.parent
+        return ancestors
+
+    @property
+    def siblings(self):
+        if not self.parent:
+            return []
+        else:
+            return [topic for topic in self.parent.children if topic != self]
+
+    @property
+    def content(self):
+        if self.id in correlations_df.index:
+            return [ContentItem(content_id) for content_id in correlations_df.loc[self.id].content_ids.split()]
+        else:
+            return tuple([]) if self.has_content else []
+
+    def get_breadcrumbs(self, separator=" >> ", include_self=True, include_root=True):
+        ancestors = self.ancestors
+        if include_self:
+            ancestors = [self] + ancestors
+        if not include_root:
+            ancestors = ancestors[:-1]
+        return separator.join(reversed([a.title for a in ancestors]))
+
+    @property
+    def children(self):
+        return [Topic(child_id) for child_id in topics_df[topics_df.parent == self.id].index]
+
+    def subtree_markdown(self, depth=0):
+        markdown = "  " * depth + "- " + self.title + "\n"
+        for child in self.children:
+            markdown += child.subtree_markdown(depth=depth + 1)
+        for content in self.content:
+            markdown += ("  " * (depth + 1) + "- " + "[" + content.kind.title() + "] " + content.title) + "\n"
+        return markdown
+
+    def __eq__(self, other):
+        if not isinstance(other, Topic):
+            return False
+        return self.id == other.id
+
+    def __getattr__(self, name):
+        return topics_df.loc[self.id][name]
+
+    def __str__(self):
+        return self.title
+    
+    def __repr__(self):
+        return f"<Topic(id={self.id}, title=\"{self.title}\")>"
+
+
+class ContentItem:
+    def __init__(self, content_id):
+        self.id = content_id
+
+    @property
+    def topics(self):
+        return [Topic(topic_id) for topic_id in topics_df.loc[correlations_df[correlations_df.content_ids.str.contains(self.id)].index].index]
+
+    def __getattr__(self, name):
+        return content_df.loc[self.id][name]
+
+    def __str__(self):
+        return self.title
+    
+    def __repr__(self):
+        return f"<ContentItem(id={self.id}, title=\"{self.title}\")>"
+
+    def __eq__(self, other):
+        if not isinstance(other, ContentItem):
+            return False
+        return self.id == other.id
+
+    def get_all_breadcrumbs(self, separator=" >> ", include_root=True):
+        breadcrumbs = []
+        for topic in self.topics:
+            new_breadcrumb = topic.get_breadcrumbs(separator=separator, include_root=include_root)
+            if new_breadcrumb:
+                new_breadcrumb = new_breadcrumb + separator + self.title
+            else:
+                new_breadcrumb = self.title
+            breadcrumbs.append(new_breadcrumb)
+        return breadcrumbs
